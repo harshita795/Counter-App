9e181f997d9c34d61880ef11aec5fab719b01786
harshita795 <yadavharshita261303@gmail.com>
2023-08-27 22:36:27 -0800

    Count color changed randomly

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 (function() {
   const buttons = document.querySelectorAll('.counterBtn')
+  
   let count = 0
 
   buttons.forEach(function(button) {
@@ -10,15 +11,23 @@
         else if(button.classList.contains('nextBtn')){
            count++ 
         }
-
+        
+        
         const counter = document.querySelector('#counter')
         counter.textContent = count
-
+      
+        
         if(count < 0){
-            counter.style.color = 'red'
+          const color = ['#cd5c5c','green','#fffof5','#add8e6','pink','gray','purple','cyan','fuchsia','brown','coral','crimson','indigo','teal','olive','maroon','antiqueWhite','azure','beige','cadetBlue','chocolate','DarkSlateBlue'];
+
+            const colorIndex = parseInt(Math.random()*color.length)
+            counter.style.color = color[colorIndex]
         }
         else if(count > 0){
-            counter.style.color = 'green'
+          const color = ['#cd5c5c','green','#fffof5','#add8e6','pink','gray','purple','cyan','fuchsia','brown','coral','crimson','indigo','teal','olive','maroon','antiqueWhite','azure','beige','cadetBlue','chocolate','DarkSlateBlue'];
+
+            const colorIndex = parseInt(Math.random()*color.length)
+            counter.style.color = color[colorIndex]
         }
         else{
             counter.style.color = '#333333'
